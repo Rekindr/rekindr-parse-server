758afe4ce2db66a7417d703cafc28539ffee10f0
Mai Anh Vu <me@maianhvu.com>
2017-04-29 13:15:04 -0700

    update cloud functions

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,14 @@
-Parse.Cloud.define("hello", (req, res) => {
-    res.success("Hello World!");
+// Class definitions
+const Baby = Parse.Object.extend("Baby");
+const Photo = Parse.Object.extend("Photo");
+
+Parse.Cloud.define("getBabies", (req, res) => {
+    let babyQuery = new Parse.Query(Baby).equalTo('parent', req.user);
+    babyQuery.find().then(babies => res.success(babies));
+});
+
+Parse.Cloud.define("uploadPhoto", (req, res) => {
+    let photo = new Photo();
+    photo.set('caption', req.params.caption);
+    photo.save().then(result => res.success(result));
 })
