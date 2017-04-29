14e34ac20b01dac473cc027e605356d3b01f6aff
Mai Anh Vu <me@maianhvu.com>
2017-04-29 13:22:44 -0700

    add original uploading

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,7 @@
 // Class definitions
 const Baby = Parse.Object.extend("Baby");
 const Photo = Parse.Object.extend("Photo");
+const PhotoBabyTag = Parse.Object.extend("PhotoBabyTag");
 
 Parse.Cloud.define("getBabies", (req, res) => {
     let babyQuery = new Parse.Query(Baby).equalTo('parent', req.user);
@@ -10,5 +11,8 @@ Parse.Cloud.define("getBabies", (req, res) => {
 Parse.Cloud.define("uploadPhoto", (req, res) => {
     let photo = new Photo();
     photo.set('caption', req.params.caption);
-    photo.save().then(result => res.success(result));
+    photo.set('original', req.params.original);
+    photo.save().then(savedPhoto => {
+        res.success(savedPhoto);
+    });
 })
