cc1e483d971adcfd2f3949cb1c11adca7d344c5c
Mai Anh Vu <me@maianhvu.com>
2017-04-29 13:39:18 -0700

    add tagging

--- cloud/main.js
+++ cloud/main.js
@@ -12,7 +12,18 @@ Parse.Cloud.define("uploadPhoto", (req, res) => {
     let photo = new Photo();
     photo.set('caption', req.params.caption);
     photo.set('original', req.params.original);
+    photo.set('uploader', req.user);
     photo.save().then(savedPhoto => {
-        res.success(savedPhoto);
-    });
+        let tags = Object.keys(req.params.babies).map(babyId => {
+            let tag = new PhotoBabyTag();
+            tag.set('baby', Baby.createWithoutData(babyId));
+            tag.set('photo', savedPhoto);
+            tag.set('faceLeft', babies[babyId][0]);
+            tag.set('faceTop', babies[babyId][1]);
+            tag.set('faceRight', babies[babyId][2]);
+            tag.set('faceBottom', babies[babyId][3]);
+            return tag;
+        });
+        return Parse.Promise.when(tags.map(tag => tag.save()));
+    }).then(() => res.success(photo));
 })
