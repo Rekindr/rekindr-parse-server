47b4c509fe39dfed875efd5e90aa9a718f9f0a33
Mai Anh Vu <me@maianhvu.com>
2017-04-29 11:38:01 -0700

    modify settings and add cloud code

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ const api = new ParseServer({
     cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
     appId: process.env.APP_ID || 'rekindr',
     masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
-    serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
     // liveQuery: {
     //     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
     // }
