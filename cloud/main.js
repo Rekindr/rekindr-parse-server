38c8d6cb8251a1dac68246ccbd048ac330c7771f
Mai Anh Vu <me@maianhvu.com>
2017-04-29 17:02:49 -0700

    add news feed function

--- cloud/main.js
+++ cloud/main.js
@@ -2,6 +2,7 @@
 const Baby = Parse.Object.extend("Baby");
 const Photo = Parse.Object.extend("Photo");
 const PhotoBabyTag = Parse.Object.extend("PhotoBabyTag");
+const Family = Parse.Object.extend("Family");
 
 Parse.Cloud.define("getBabies", (req, res) => {
     let babyQuery = new Parse.Query(Baby).equalTo('parent', req.user);
@@ -23,4 +24,31 @@ Parse.Cloud.define("uploadPhoto", (req, res) => {
         });
         return Parse.Promise.when(tags.map(tag => tag.save()));
     }).then(() => res.success(photo));
-})
+});
+
+Parse.Cloud.define("getNewsFeed", (req, res) => {
+    new Parse.Query(Family).equalTo('members', req.user).find().then(families => {
+        return Parse.Promise.when(families.map(family => family.get('babies').query().find()));
+    }).then(results => {
+        let babies = results.reduce((a, v) => a.concat(v), []);
+        return new Parse.Query(PhotoBabyTag).containedIn('baby', babies)
+            .include('photo')
+            .include('baby')
+            .limit(30).find();
+    }).then(tags => {
+        let photos = {};
+        tags.forEach(tag => {
+            let photoId = tag.get('photo').id;
+            if (!photos.hasOwnProperty(photoId)) {
+                photos[photoId] = {
+                    photo: tag.get('photo'),
+                    tags: []
+                };
+            }
+            photos[photoId].tags.push(tag);
+        });
+        let result = Object.keys(photos).map(photoId => photos[photoId])
+            .sort((a, b) => b.photo.get('createdAt') - a.photo.get('createdAt'));
+        res.success(result);
+    }).fail(err => res.error(err));
+});
